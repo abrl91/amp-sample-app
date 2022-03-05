51a656e87a31eecd06a1f20715b924f69cee4a73
Amit Barletz <39680385+abrl91@users.noreply.github.com>
2022-03-05 21:52:32 +0200

    amplication-build-cl0e9i8hm154233501s6rzadvyw2

--- admin-ui/src/user/roles.ts
+++ admin-ui/src/user/roles.ts
@@ -3,4 +3,16 @@ export const ROLES = [
     name: "user",
     displayName: "User",
   },
+  {
+    name: "regular",
+    displayName: "Regular",
+  },
+  {
+    name: "admin",
+    displayName: "Admin",
+  },
+  {
+    name: "manager",
+    displayName: "Manager",
+  },
 ];
